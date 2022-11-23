0dabb7bb0e699046394b2e7e2867a4ef11b27028
Chuzas <lucianux@gmail.com>
2022-11-23 15:23:17 -0300

    Fix on Swagger config
    Fixed Swagger configuration. Since the secured endpoints could not be tested because swagger was not configured to be able to enter the token.

--- source/Web/Program.cs
+++ source/Web/Program.cs
@@ -6,13 +6,40 @@ using DotNetCore.IoC;
 using DotNetCore.Logging;
 using DotNetCore.Security;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder();
 
 builder.Host.UseSerilog();
 
 builder.Services.AddResponseCompression();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer",
+        new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter into field the word 'Bearer' following by space and JWT",
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey
+        });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 builder.Services.AddSpaStaticFiles("Frontend");
 builder.Services.AddAuthenticationJwtBearer(new JwtSettings(Guid.NewGuid().ToString(), TimeSpan.FromHours(12)));
 builder.Services.AddContext<Context>(options => options.UseSqlServer(builder.Services.GetConnectionString(nameof(Context))));
